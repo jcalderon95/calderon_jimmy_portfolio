ca51aa950be993cae47c288ddab4af92c9911b2b
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-12-06 03:41:49 -0500

    updated home section

--- js/main.js
+++ js/main.js
@@ -4,24 +4,23 @@
 
     const menu = document.querySelector('.menuCon'),
           navLightbox = document.querySelector('.navLightbox');
-
     
 
     function openNav(){
         let navLink = document.querySelectorAll('.navLink');
         navLightbox.classList.add('navLightboxOn'); 
 
+
         let myTL = new TimelineLite();
 
         myTL.from(navLightbox, 0.4 ,{ opacity: 0})
-            // .from(navLink, 0.1, { opacity: 0})
+            //.from(navLink, 0.1, { opacity: 0})
     }
 
 
     // nav Lighgbox
     menu.addEventListener('click', openNav);
     navLightbox.querySelector('.navClose').addEventListener('click', function(){
-        // tlNav.reversed() ? tlNav.play() : tlNav.reverse();
         navLightbox.classList.remove('navLightboxOn');
     })
     
