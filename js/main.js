5902e317c31039c54b1c993bd3be8062b25900ad
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-12-06 17:17:32 -0500

    updated contact section

--- js/main.js
+++ js/main.js
@@ -4,17 +4,24 @@
 
     const menu = document.querySelector('.menuCon'),
           navLightbox = document.querySelector('.navLightbox');
+
+          
     
 
     function openNav(){
-        let navLink = document.querySelectorAll('.navLink');
+        let navLink = document.querySelectorAll('.navLink'),
+            closeNav = document.querySelector('.navClose');
+        
         navLightbox.classList.add('navLightboxOn'); 
 
+        var tl = new TimelineMax();
 
-        let myTL = new TimelineLite();
+        // tl.to(navLightbox, 0.5, { opacity: 0})
+         tl.from(navLightbox, 0.6, { opacity: 0}, { opacity: 1,  ease: Power0.easeNone})
+          .staggerFrom(navLink, 1.5, {opacity:0, ease: Power1.easeIn}, 0.2)
+          .from(closeNav, 0.2, {opacity: 0, ease: Power1.easeIn });
 
-        myTL.from(navLightbox, 0.4 ,{ opacity: 0})
-            //.from(navLink, 0.1, { opacity: 0})
+        
     }
 
 
